c8f93f2b82455dc619d001ee8cb27464b242e53f
Nick Strupat <nick.strupat@gmail.com>
2018-06-18 23:27:23 -0400

    what

--- Program.cs
+++ Program.cs
@@ -28,50 +28,55 @@ namespace TaxiFarePrediction
                 typeof(PoissonRegressor),
                 typeof(StochasticDualCoordinateAscentRegressor),
             };
-            // var taxiTripTests = File.ReadAllLines(_testdatapath)
-            //                         .Skip(1)
-            //                         .Select(x => {
-            //                             var fields = x.Split(',');
-            //                             var taxiTrip = new TaxiTrip {
-            //                                 VendorId = fields[0],
-            //                                 RateCode = fields[1],
-            //                                 PassengerCount = float.Parse(fields[2]),
-            //                                 TripTime = float.Parse(fields[3]),
-            //                                 TripDistance = float.Parse(fields[4]),
-            //                                 PaymentType = fields[5],
-            //                                 FareAmount = float.Parse(fields[6])
-            //                             };
-            //                             return (taxiTrip, taxiTripFarePrediction:(TaxiTripFarePrediction)null);
-            //                         })
-            //                         .ToList();
+
+            var taxiTripTests = File.ReadAllLines(_testdatapath)
+                                    .Skip(1)
+                                    .Select(x => {
+                                        var fields = x.Split(',');
+                                        var taxiTrip = new TaxiTrip {
+                                            VendorId = fields[0],
+                                            RateCode = fields[1],
+                                            PassengerCount = float.Parse(fields[2]),
+                                            TripTime = float.Parse(fields[3]),
+                                            TripDistance = float.Parse(fields[4]),
+                                            PaymentType = fields[5],
+                                            FareAmount = float.Parse(fields[6])
+                                        };
+                                        return (taxiTrip, taxiTripFarePrediction:(TaxiTripFarePrediction)null);
+                                    })
+                                    //.Take(500)
+                                    .ToArray();
 
             Func<PredictionModel<TaxiTrip, TaxiTripFarePrediction>> trainFunc = Train<FastForestRegressor>;
             foreach (var regressorType in regressorTypes)
             {
                 var methodInfo = trainFunc.Method.GetGenericMethodDefinition().MakeGenericMethod(regressorType);
                 Console.WriteLine();
-                Console.WriteLine(regressorType.Name);
+                Console.WriteLine("--- " + regressorType.Name + " ---");
                 var model = (PredictionModel<TaxiTrip, TaxiTripFarePrediction>) methodInfo.Invoke(null, null);
                 Evaluate(model);
                 var prediction = model.Predict(TestTrips.Trip1);
                 Console.WriteLine("Predicted fare: {0}, actual fare: 29.5", prediction.FareAmount);
-                // Console.WriteLine();
-                // Console.Write("Begin test...");
-                // for (var i = 0; i != taxiTripTests.Count; i++)
-                // {
-                //     if (i % 50_000 == 0)
-                //         Console.Write(".");
-                //     var taxiTripTest = taxiTripTests[i];
-                //     var pred = model.Predict(taxiTripTest.taxiTrip);
-                //     taxiTripTest.taxiTripFarePrediction = pred;
-                // }
-                // Console.WriteLine("Done.");
-                // File.WriteAllLines(@".\Data\ModelErrors.txt", taxiTripTests.Select(x =>
-                //     (x.taxiTrip.FareAmount - x.taxiTripFarePrediction.FareAmount).ToString()
-                // ));
-                // Console.WriteLine(taxiTripTests.Min(x => x.taxiTrip.FareAmount - x.taxiTripFarePrediction.FareAmount));
-                // Console.WriteLine(taxiTripTests.Max(x => x.taxiTrip.FareAmount - x.taxiTripFarePrediction.FareAmount));
-                // Console.WriteLine(taxiTripTests.Average(x => x.taxiTrip.FareAmount - x.taxiTripFarePrediction.FareAmount));
+
+                Console.WriteLine();
+                Console.Write("Begin test...");
+                var what = model.Predict(taxiTripTests.Select(x => x.taxiTrip)).ToList();
+                Parallel.ForEach(what, (x,state, i) => taxiTripTests[(int)i].taxiTripFarePrediction = x);
+                
+                Console.WriteLine("Done.");
+                var diffs = taxiTripTests.Select(x => Math.Abs(x.taxiTrip.FareAmount - x.taxiTripFarePrediction.FareAmount))
+                                         .AsParallel()
+                                         .OrderBy(x => x)
+                                         .ToList();
+                var okay = diffs.Take(diffs.Count - 2)
+                                .Where((x, i) => x > 0.75 & x < 450)
+                                .Where((x, i) => i % 1000 == 0)
+                                .Select((x, i) => i + "," + x)
+                                .ToList();
+                File.WriteAllLines($@".\Data\{regressorType.Name}_ModelErrors.csv", okay);
+                Console.WriteLine(taxiTripTests.Min(x => Math.Abs(x.taxiTrip.FareAmount - x.taxiTripFarePrediction.FareAmount)));
+                Console.WriteLine(taxiTripTests.Max(x => Math.Abs(x.taxiTrip.FareAmount - x.taxiTripFarePrediction.FareAmount)));
+                Console.WriteLine(taxiTripTests.Average(x => Math.Abs(x.taxiTrip.FareAmount - x.taxiTripFarePrediction.FareAmount)));
             }
         }
 
@@ -79,6 +84,8 @@ namespace TaxiFarePrediction
         where TRegressor : ILearningPipelineItem, new()
         {
             var modelLoaded = PredictionModel.ReadAsync<TaxiTrip, TaxiTripFarePrediction>(Path.Combine(BasePath, typeof(TRegressor).Name) + ".zip").Result;
+            return modelLoaded;
+
             //var what = modelLoaded.Predict()
             var pipeline = new LearningPipeline
             {
